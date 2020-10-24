37d763d9bc79cc71d41d918979e43c1b5449ebf9
Dima Kabarukhin <thedimzone99@gmail.com>
2020-10-24 19:16:35 +0300

    drafts for test system

--- src/app/components/test-page/test-page.component.ts
+++ src/app/components/test-page/test-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Test, Question, Answer } from "../../models/test";
+import tests from "../../tests/";
 
 @Component({
   selector: 'app-test-page',
@@ -7,12 +9,28 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./test-page.component.css']
 })
 export class TestPageComponent implements OnInit {
+  public test: Test;
+  public current_question: Question;
+  public current_step: number = 0;
+  public current_step_percent: number = 50;
 
   constructor(private titleService: Title ) {
     this.titleService.setTitle("Тест");
+
+    this.test = tests.test1 as Test;
+    this.current_question = this.test.questions[0];
+
   }
 
   ngOnInit(): void {
+  }
+
+  next_question(): void {
+    this.current_step++;
+    //if(this.current_step == this.test.questions.length) //... finish
+
+    this.current_question = this.test.questions[this.current_step];
+    this.current_step_percent = this.current_step / this.test.questions.length * 100;
   }
 
 }
---
+++ src/app/tests/index.ts
@@ -0,0 +1,5 @@
+import test1 from './test1.json';
+
+//var tests = [ test1 ];
+
+export default { test1: test1 };

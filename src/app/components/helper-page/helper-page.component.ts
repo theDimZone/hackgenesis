2ae517d8abe9be37d341bbab8e7e760643366185
Dima Kabarukhin <thedimzone99@gmail.com>
2020-10-25 04:04:28 +0300

    helper backend

--- src/app/components/helper-page/helper-page.component.ts
+++ src/app/components/helper-page/helper-page.component.ts
@@ -23,24 +23,54 @@ export class HelperPageComponent implements OnInit {
 
     this.user = this.localStorageService.getUser();
     console.log(this.user);
-
-    //let stocks = db.stocks;
-    //console.log(stocks);
   }
 
-  ngOnInit(): void {
-    let stocks = db.stocks as Offer[];
-    for(let stock of stocks) {
-        for(let stock_factor in stock.required_factors) {
-          if(stock.required_factors[stock_factor] < this.user.factors[stock_factor]) {
-            this.best_stocks.push(stock);
+  findBestOffers(offers: Offer[]): Offer[] {
+    let best_offers: Offer[] = [];
+
+    for(let offer of offers) {
+        for(let offer_factor in offer.required_factors) {
+          if(offer.required_factors[offer_factor] < this.user.factors[offer_factor]) {
+            best_offers.push(offer);
           } else {
             continue;
           }
         }
     }
 
-    this.best_stocks.sort((a, b) => a.possible_profit - b.possible_profit);
+    best_offers.sort((a, b) => a.possible_profit - b.possible_profit);
+    return best_offers;
+  }
+
+  findBestProducts(products: Product[]): Product[] {
+    let best_products: Product[] = [];
+
+    for(let product of products) {
+        for(let product_factor in product.required_factors) {
+          if(product.required_factors[product_factor] < this.user.factors[product_factor]) {
+            best_products.push(product);
+          } else {
+            continue;
+          }
+        }
+    }
+
+    best_products.sort((a, b) => a.possible_profit - b.possible_profit);
+    return best_products;
+  }
+
+  ngOnInit(): void {
+    let stocks = db.stocks as Offer[];
+    this.best_stocks = this.findBestOffers(stocks);
+
+    let bonds = db.bonds as Offer[];
+    this.best_bonds = this.findBestOffers(bonds);
+
+    let products = db.products as Product[];
+    this.best_products = this.findBestProducts(products);
+
+
+
     console.log(this.best_stocks);
 
   }
